3227aedb4ccab248b58aaf335a23bed7787d9797
KusalDemo <kusaldemo2022@gmail.com>
2024-07-27 17:49:32 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import {NicDetailCheckerCard} from "./component/nicDetailCheckerCard/nicDetailCheckerCard.jsx";
+
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    /*<section>
+        <h1>Hello ReactJS</h1>
+    </section>*/
+      <section>
+          <div>
+              <h1>Nic Detail Checker</h1>
+              <NicDetailCheckerCard/>
+          </div>
+      </section>
+  )
+}
+
+export default App
